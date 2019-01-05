2e0fb740d82245594c437aab981a97e4a4788ca1
Jungbin Kim <mail@jungbin.kim>
2019-01-05 21:47:57 +0900

    Split into default project and project2 using vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg" />
   </div>
 </template>
 
@@ -10,6 +9,11 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
+  data() {
+    return {
+      msg: process.env.VUE_APP_MODE
+    };
+  },
   components: {
     HelloWorld
   }
