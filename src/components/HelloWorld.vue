a6191717d94fb4b420bae0ee01df2a06fddd91fe
Jungbin Kim <mail@jungbin.kim>
2019-01-05 22:03:21 +0900

    Add some data set

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,49 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        {{ item.text }}
+        <ul v-if="item.innerList && item.innerList.length > 0">
+          <li v-for="(l, k) in item.innerList" :key="k">
+            {{ l.text }}
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  data() {
+    return {
+      list: [
+        {
+          text: "item1",
+          innerList: [
+            { text: "1-1 Apple1" },
+            { text: "1-2 Apple2" },
+            { text: "1-3 Apple3" },
+            { text: "1-4 Apple4" }
+          ]
+        },
+        {
+          text: "item2"
+        },
+        {
+          text: "item3",
+          innerList: [
+            { text: "3-1 Banana1" },
+            { text: "3-2 Banana2" },
+            { text: "3-3 Banana3" },
+            { text: "3-4 Banana4" }
+          ]
+        }
+      ]
+    };
+  },
   props: {
     msg: String
   }
@@ -23,7 +60,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
